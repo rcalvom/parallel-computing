8972c65d7749c0d80c3d5e5ad23b214495d943a1
Ricardo Andrés Calvo <rcalvom@unal.edu.co>
2022-03-20 17:17:51 -0500

    Pi con gráficas

--- pi/main.c
+++ pi/main.c
@@ -1,5 +1,8 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 
 int threads_num = 0;
@@ -7,36 +10,43 @@ double pi = 0;
 double *pi_array;
 long iterations = 0;
 
-void *calc(void* ap){                                                       // Función que calcula una suma parcial del calculo de Pi.
+void *calc(void* ap){
     double pi_local  = 0;
-    long countS = iterations / threads_num * (*(int*)ap) + 1;               // Teniendo en cuenta el número de hilo, calcula los limites de la sumatoria.
+    long countS = iterations / threads_num * (*(int*)ap) + 1;
     long countE = iterations / threads_num * (*(int*)ap + 1) + 1;           
-    for(long i = countS; i < countE;i++){                                   // Se calcula Pi usando los limites correspondientes al hilo.
+    for(long i = countS; i < countE;i++){
         pi_local += (4.0) / (2 * i - 1);
         i++;
         pi_local -= (4.0) / (2 * i - 1);
     }
-    *(pi_array + (*(int*)ap)) = pi_local;                                   // Se guarda el valor obtenido de la suma parcial en una posición de pi_array.
+    *(pi_array + (*(int*)ap)) = pi_local;
 }
 
 
-int main(int argc, char* argv[]){ 
-    threads_num = atoi(argv[1]);                                            // Obtenemos el número de hilos a utilizar.
-    iterations = atol(argv[2]);                                             // Obtenemos el número de iteraciones a realizar.
-    pi_array = malloc(sizeof(double) * threads_num);                        // Se declara un espacio de memoria donde cada hilo almacenará una suma parcial.
-    int data[threads_num];                                                  // Se declara un arreglo para indicarle a los hilos que parte de la suma van a realizar.
-    pthread_t tfd[threads_num];                                             // Creamos el arreglo de hilos.
+int main(int argc, char* argv[]){
+    struct timeval start;
+    struct timeval end; 
+    double stopwatch;
+    gettimeofday(&start, NULL);
+    threads_num = atoi(argv[1]);
+    iterations = atol(argv[2]);
+    pi_array = malloc(sizeof(double) * threads_num);
+    int data[threads_num];
+    pthread_t tfd[threads_num];
     for(int i = 0; i < threads_num; i++){
-        data[i] = i;                                                
-        pthread_create(&tfd[i], NULL, calc, &data[i]);                      // Se crean los hilos.
+        data[i] = i;
+        pthread_create(&tfd[i], NULL, calc, &data[i]);
     }
-    for(int i = 0; i < threads_num; i++){                                   // Se espera a que todos los hijos hayan terminado.
+    for(int i = 0; i < threads_num; i++){
         pthread_join(tfd[i], NULL);
     }
-    for(int i = 0; i < threads_num; i++){                                   // Se suman todas la sumas parciales en a variable Pi
+    for(int i = 0; i < threads_num; i++){
         pi += *(pi_array + i);
     }
-    printf("The calculated value of pi is: %1.12f\n\n", pi);                // Se imprime el valor obtenido en consola.      
-    free(pi_array);                                                         // Se librera espacio de memoria ya no utilizado.
+    gettimeofday(&end, NULL);
+    stopwatch = (double)(end.tv_sec + (double) end.tv_usec / 1000000) - (double)(start.tv_sec + (double)start.tv_usec / 1000000);
+    printf("The calculated value of pi is: %1.12f\n\n", pi);
+    printf("The excecution of the program has taken: %.16g miliseconds.\n", stopwatch * 1000.0);
+    free(pi_array);
     return 0;
 }
---
+++ pi/speedup.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from matplotlib.collections import EventCollection
+import numpy as np
+
+list_1 = [6340.088844299316, 6335.758924484253, 6425.503969192505, 6347.927808761597, 6348.183155059814]
+list_2 = [3340.939998626709, 3318.953037261963, 3334.092855453491, 3271.924018859863, 3390.055894851685]
+list_4 = [2853.395223617554, 2846.215009689331, 2873.318195343018, 2871.086120605469, 2775.845050811768]
+list_8 = [2862.823009490967, 2869.314908981323, 2902.832984924316, 2963.523864746094, 2813.30418586731]
+list_16 = [2906.139135360718, 2706.709146499634, 2587.829113006592, 2684.438943862915, 2896.565198898315]
+
+time_1 = np.average(list_1)
+time_2 = np.average(list_2)
+time_4 = np.average(list_4)
+time_8 = np.average(list_8)
+time_16 = np.average(list_16)
+
+threads = [1, 2, 4, 8, 16]
+times = [time_1/time_1, time_1/time_2, time_1/time_4, time_1/time_8, time_1/time_16]
+
+fig, ax = plt.subplots()
+ax.plot(threads, times)
+
+ax.set(xlabel='Número de Hilos', ylabel='Tiempo (ms)', title='Número de hilos vs Speed up')
+ax.grid()
+
+fig.savefig("speedup.png")

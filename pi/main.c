fb54e5895f51fcdd308267969e2d8f29b2c8b046
Ricardo Andrés Calvo <rcalvom@unal.edu.co>
2022-03-21 18:31:24 -0500

    formatting

--- pi/main.c
+++ pi/main.c
@@ -7,14 +7,14 @@
 
 int threads_num = 0;
 double pi = 0;
-double *pi_array;
+double* pi_array;
 long iterations = 0;
 
-void *calc(void* ap){
-    double pi_local  = 0;
+void* calc(void* ap) {
+    double pi_local = 0;
     long countS = iterations / threads_num * (*(int*)ap) + 1;
-    long countE = iterations / threads_num * (*(int*)ap + 1) + 1;           
-    for(long i = countS; i < countE;i++){
+    long countE = iterations / threads_num * (*(int*)ap + 1) + 1;
+    for (long i = countS; i < countE;i++) {
         pi_local += (4.0) / (2 * i - 1);
         i++;
         pi_local -= (4.0) / (2 * i - 1);
@@ -23,9 +23,9 @@ void *calc(void* ap){
 }
 
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
     struct timeval start;
-    struct timeval end; 
+    struct timeval end;
     double stopwatch;
     gettimeofday(&start, NULL);
     threads_num = atoi(argv[1]);
@@ -33,18 +33,18 @@ int main(int argc, char* argv[]){
     pi_array = malloc(sizeof(double) * threads_num);
     int data[threads_num];
     pthread_t tfd[threads_num];
-    for(int i = 0; i < threads_num; i++){
+    for (int i = 0; i < threads_num; i++) {
         data[i] = i;
         pthread_create(&tfd[i], NULL, calc, &data[i]);
     }
-    for(int i = 0; i < threads_num; i++){
+    for (int i = 0; i < threads_num; i++) {
         pthread_join(tfd[i], NULL);
     }
-    for(int i = 0; i < threads_num; i++){
+    for (int i = 0; i < threads_num; i++) {
         pi += *(pi_array + i);
     }
     gettimeofday(&end, NULL);
-    stopwatch = (double)(end.tv_sec + (double) end.tv_usec / 1000000) - (double)(start.tv_sec + (double)start.tv_usec / 1000000);
+    stopwatch = (double)(end.tv_sec + (double)end.tv_usec / 1000000) - (double)(start.tv_sec + (double)start.tv_usec / 1000000);
     printf("The calculated value of pi is: %1.12f\n\n", pi);
     printf("The excecution of the program has taken: %.16g miliseconds.\n", stopwatch * 1000.0);
     free(pi_array);
